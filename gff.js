26aecb2246fa394775ac73d7d5a15a10f12226db
lotuytea <30753375+lotuytea@users.noreply.github.com>
2017-10-16 00:29:34 +0700

    Add files via upload

---
+++ gff.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+<!-- 
+document.writeln(" <!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\"> "); 
+document.writeln(" <html> "); 
+document.writeln(" <head> "); 
+document.writeln(" <title>Blog Waroengweb</title> "); 
+document.writeln(" </head> "); 
+document.writeln(" <frameset rows=\"*\"> "); 
+document.writeln(" <frame frameborder=0 src=\"http://www.livestreamingonline.co/2017/09/bein4.html\" scrolling=\"auto\" noresize> "); 
+document.writeln(" </frameset> "); 
+document.writeln(" </html> ");
+ // -->
+</script>
