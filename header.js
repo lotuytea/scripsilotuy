87e1ae2e1790e5a83d7d1c5ed54fa178bfb35aee
lotuytea <30753375+lotuytea@users.noreply.github.com>
2017-10-26 23:09:39 +0700

    Delete header.js

--- header.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
- //<![CDATA[
- function embed(video) {
-    document.write('<div style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"><iframe " src="http://www.livestreamingonline.co/');
-    document.write(video);
-    document.write('" frameborder="0" allowfullscreen="1"></iframe></div></div>');
- }
- //]]>
- </script>
